aa5aa2d5ba546c9e26832120e72438c5ac3de239
qalbehabib <rana.habib313@gmail.com>
2022-09-27 12:57:28 +0500

    updated database

--- config/db-config.js
+++ config/db-config.js
@@ -1,8 +1,13 @@
-var mongoose = require('mongoose')
-var db_url = 'mongodb+srv://root:root@cluster0.fkhclig.mongodb.net/onlineIDE?retryWrites=true&w=majority'
+var mongoose = require("mongoose");
+var db_url =
+  "mongodb+srv://root:root@cluster0.le6kde0.mongodb.net/IDE?retryWrites=true&w=majority";
 
 // var db_url = 'mongodb://wajid:codeeditor543@ds263436.mlab.com:63436/codeeditor'
-mongoose.connect(db_url, { useNewUrlParser: true , useUnifiedTopology: true })
+mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'DB connection error:'));
-db.once('open', function () { console.log('Successfully connected to DB') });
+db.on("error", console.error.bind(console, "DB connection error:"));
+db.once("open", function () {
+  console.log("Successfully connected to DB");
+});
+
+// mongodb+srv://root:root@cluster0.fkhclig.mongodb.net/onlineIDE?retryWrites=true&w=majority
